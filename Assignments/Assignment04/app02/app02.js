f09b67940d53ba9299f3c5442d0d8d331bbd5973
Jarobec <48001639+Jarobec@users.noreply.github.com>
2022-05-26 18:58:45 -0500

    added assignment 4

---
+++ Assignments/Assignment04/app02/app02.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+require("./api/data/db.connection").open();
+const express = require("express");
+const path = require("path");
+const routes = require("./api/routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  console.log(`Method: ${req.method}, Url: ${req.url}`);
+  next();
+});
+
+app.use("/api", routes);
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(process.env.PORT, function () {
+  console.log("Server is running on port " + server.address().port);
+});
